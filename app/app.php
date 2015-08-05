b20c010bb3e1cbbd4974d5e07ba0e0559adcc5bc
Dana Sharman & Sarah McEwing <student@epicodus.com>
2015-08-05 11:59:02 -0700

    Created ToDoList

---
+++ app/app.php
@@ -0,0 +1,32 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+
+    session_start();
+    if (empty($_SESSION['list_of_tasks'])) {
+      $_SESSION['list_of_tasks'] = array();
+    }
+
+
+    $app = new Silex\Application();
+
+    $app->get("/", function(){
+      $test_task = new Task("Learn PHP.");
+      $another_test_task = new Task("Learn Drupal.");
+      $third_task = new Task("Visit France.");
+
+      $list_of_tasks = array($test_task, $another_test_task, $third_task);
+      $output = "";
+
+      foreach ($list_of_tasks as $task) {
+        $output = $output . "<p>" . $task->getDescription() . "</p>";
+      }
+
+      return $output;
+
+    });
+
+
+
+    return $app;
+?>
