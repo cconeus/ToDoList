d2ea43f98159619e4ed08ce4ed2c108ad44bf7b7
Dana Sharman <danasharman@Danas-iMac.local>
2015-08-26 14:51:43 -0700

    added tests

--- src/Task.php
+++ src/Task.php
@@ -1,44 +1,45 @@
 <?php
-class Task
-{
-    private $description;
-
-    function __construct($description)
+    class Task
     {
-        $this->description = $description;
-    }
 
-    function setDescription($new_description)
-    {
-        $this->description = (string) $new_description;
-    }
+        private $description;
 
-    function getDescription()
-    {
-        return $this->description;
-    }
-
-    function save()
-    {
-        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
-    }
-
-    static function getAll()
-    {
-        returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
-        $tasks = array();
-        foreach($returned_tasks as $task) {
-            $description = $task['description'];
-            $new_task = new Task($description);
-            array_push($tasks, $new_task);
+        function __construct($description)
+        {
+            $this->description = $description;
         }
-        return $tasks;
-    }
 
-    static function deleteAll()
-    {
-        $_SESSION['list_of_tasks'] = array();
-    }
+        function setDescription($new_description)
+        {
+            $this->description = (string) $new_description;
+        }
 
-}
+        function getDescription()
+        {
+            return $this->description;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+        }
+
+        static function getAll()
+        {
+            returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+            $tasks = array();
+            foreach($returned_tasks as $task) {
+                $description = $task['description'];
+                $new_task = new Task($description);
+                array_push($tasks, $new_task);
+            }
+            return $tasks;
+        }
+
+        static function deleteAll()
+        {
+            $_SESSION['list_of_tasks'] = array();
+        }
+
+    }
 ?>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit014cd0533471491ab3091cdaca5875a2
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit014cd0533471491ab3091cdaca5875a2', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
